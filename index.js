d678ddd866a42a69aaaa7b4b8c7ee503553af29f
Just_Nefarious <f2v74yt@gmail.com>
2023-03-01 17:16:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const cors = require('cors');
 
 
 app.use(cors());
+app.use(express.json({ extended: false }));
+
+app.use("/api/product", (data) => {
+    console.log(data);
+});
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
 })
 
+module.exports = app;
